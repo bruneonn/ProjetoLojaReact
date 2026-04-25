4a9abbb1780fd28556df6466433c8730b7267375
bruneonn <motabruno805@gmail.com>
2026-04-25 19:22:39 -0300

    Bug Vercel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ function Footer(){
             <div className={styles.footerContent}>
                 <p>&copy; 2026 - Bruno Mota - Todos os direitos reservados</p>
                 <div className={styles.socialLinks}>
-                    <a href="#" className={styles.socialLink}>📱 Instagram</a>
-                    <a href="#" className={styles.socialLink}>📘 Facebook</a>
-                    <a href="#" className={styles.socialLink}>🐦 Twitter</a>
-                    <a href="#" className={styles.socialLink}>📧 contato@megastore.com</a>
+                    <a href="" className={styles.socialLink}>📱 Instagram</a>
+                    <a href="" className={styles.socialLink}>📘 Facebook</a>
+                    <a href="" className={styles.socialLink}>🐦 Twitter</a>
+                    <a href="" className={styles.socialLink}>📧 contato@megastore.com</a>
                 </div>
             </div>
         </footer>
